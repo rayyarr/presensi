dfc7ecf00a81c274a67d65da5ea42012ead92b66
rayyarr <rayyarr773@gmail.com>
2023-06-01 19:30:42 +0700

    Memperbarui beberapa CSS untuk halaman admin

--- admin/main-admin.php
+++ admin/main-admin.php
@@ -27,9 +27,7 @@ echo '
   <!--CSS-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/>
-  <link rel="stylesheet" href="../header/style.css?';
-time();
-echo '" id="myStylesheet">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
